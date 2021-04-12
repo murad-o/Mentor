aef3b6e745f60bd373cf43d30bb92c141889043a
Murad <murad@cron.ooo>
2021-04-12 22:50:24 +0300

    Move registration into SignUpController

--- Api/Controllers/AccountController.cs
+++ Api/Controllers/AccountController.cs
@@ -1,9 +1,6 @@
 ﻿using System.Threading.Tasks;
-using AutoMapper;
 using Entities.Models;
 using MentorCore.DTO.Account;
-using MentorCore.Interfaces.Email;
-using MentorCore.Models.Email;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,41 +10,13 @@ namespace Api.Controllers
     [Route("api/v{version:apiVersion}/[controller]")]
     public class AccountController : ControllerBase
     {
-        private readonly IMapper _mapper;
         private readonly UserManager<User> _userManager;
-        private readonly IEmailSender _emailSender;
 
-        public AccountController(IMapper mapper, UserManager<User> userManager,
-            IEmailSender emailSender)
+        public AccountController(UserManager<User> userManager)
         {
-            _mapper = mapper;
             _userManager = userManager;
-            _emailSender = emailSender;
         }
 
-        [HttpPost]
-        [Route("registration")]
-        public async Task<ActionResult> Register(RegisterModel registerModel)
-        {
-            var user = _mapper.Map<User>(registerModel);
-            var userCreated = await _userManager.CreateAsync(user, registerModel.Password);
-
-            if (!userCreated.Succeeded)
-            {
-                AddModelErrors(userCreated);
-                return BadRequest(ModelState);
-            }
-
-            var emailConfirmationToken = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-            var emailConfirmationLink = Url.Action("ConfirmEmail", "Account",
-                new {email = user.Email, token = emailConfirmationToken}, Request.Scheme);
-
-            var emailMessage = new EmailMessage(user.Email, "Подтверждение почты",
-                $"Подтвердите регистрацию, перейдя по данной ссылке: {emailConfirmationLink}");
-            await _emailSender.SendAsync(emailMessage);
-
-            return Ok();
-        }
 
         [HttpGet]
         [Route("email/confirmation")]
