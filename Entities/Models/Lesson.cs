3748b96456ede3f6a406ad9b7b5af5e58fa8bc72
Murad <murad@cron.ooo>
2021-05-04 14:23:02 +0300

    Add number for Lesson entity

--- Entities/Models/Lesson.cs
+++ Entities/Models/Lesson.cs
@@ -7,6 +7,9 @@ namespace Entities.Models
         public int Id { get; set; }
 
         [Required]
+        public int Number { get; set; }
+
+        [Required]
         public string Name { get; set; }
 
         [Required]
